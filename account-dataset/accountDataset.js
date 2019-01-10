be0b19ef34e478ee04b6937f19c7d05d9a543324
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-11 05:24:24 +0530

    Resolved Test Cases

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,43 @@
-
-function accountDataset(...args) {
-  return args;
+const {
+  bankBalances,
+} = require('./dataset.json');
+// {
+//   "amount": "822370.71",
+//   "state": "ME"
+// }
+function hundredThousandairs() {
+  return bankBalances.filter(element => element.amount > 100000);
 }
 
+function datasetWithRoundedDollar() {
+  return bankBalances.map((element) => {
+    const newElement = JSON.parse(JSON.stringify(element));
+    newElement.rounded = Math.round(element.amount);
+    return newElement;
+  });
+}
+
+function sumOfBankBalances() {
+  let newSum = 0.0;
+  bankBalances.forEach((bankBalance) => {
+    newSum += bankBalance[0];
+  });
+  return newSum;
+}
+
+function sumOfInterests() {
+  return bankBalances.reduce((sum, bankBalance) => {
+    const interestWithBalance = bankBalance[0] + bankBalance[0] * 0.189;
+    return sum + interestWithBalance;
+  }, 0);
+}
+
+function higherStateSums() {}
+
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 };
