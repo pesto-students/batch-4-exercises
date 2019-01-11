e504f6e48d4e60811a726f2d82016ff91efa117a
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-12 00:44:49 +0530

    Resolve Test Failing

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,9 @@
 
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(bigIntegerString) {
+  const afterRemoveFirstNumber = bigIntegerString.split('\n');
+  afterRemoveFirstNumber.shift();
+  const bigIntegerArray = afterRemoveFirstNumber.map(integerString => parseInt(integerString, 10));
+  return bigIntegerArray.reduce((sum, bigInteger) => sum + bigInteger, 0).toFixed();
 }
 
 export {
