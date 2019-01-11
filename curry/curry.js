df2e48900e246f30c40ee604567a9ff3c5efe4a8
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-11 23:28:43 +0530

    fix: curry fn

--- curry/curry.js
+++ curry/curry.js
@@ -1,13 +1,7 @@
-function curry(fn) {
-  const fnLen = fn.length;
-  let allParams = [];
-  return function innerFunction(...params) {
-    allParams = [...allParams, ...params];
-    if (allParams.length >= fnLen) {
-      return fn(...allParams);
-    }
-    return innerFunction;
-  };
+function curry(fn, ...args) {
+  return fn.length <= args.length
+    ? fn(...args)
+    : (...moreArgs) => curry(fn, ...args, ...moreArgs);
 }
 
 export { curry };
