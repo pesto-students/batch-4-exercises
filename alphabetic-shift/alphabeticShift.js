5ba6d95c740158de64ae88007917707bdc1fd70b
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-15 04:42:09 +0530

    update: implement alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,8 +1,15 @@
-
-function alphabeticShift(...args) {
-  return args;
+const shiftChar = (ch) => {
+  let charCode = ch.charCodeAt(0);
+  charCode += 1;
+  if (charCode === 91) charCode = 65;
+  if (charCode === 123) charCode = 98;
+  return String.fromCharCode(charCode);
+};
+function alphabeticShift(str) {
+  return str
+    .split('')
+    .map(shiftChar)
+    .join('');
 }
 
-export {
-  alphabeticShift,
-};
+export { alphabeticShift };
