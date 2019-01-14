29dca23cb127fe07f3b673bca52415811f841e36
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-15 05:17:09 +0530

    update: implement promiseAllProps

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,8 +1,21 @@
-
-function promiseAllProps(...args) {
-  return args;
+async function promiseAllProps(obj) {
+  const objAry = Object.keys(obj);
+  const result = [];
+  for (const key of objAry) {
+    result.push(obj[key]);
+  }
+  try {
+    const output = await Promise.all(result);
+    const newObj = {};
+    let i = 0;
+    for (const key of objAry) {
+      newObj[key] = output[i];
+      i += 1;
+    }
+    return newObj;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
-export {
-  promiseAllProps,
-};
+export { promiseAllProps };
