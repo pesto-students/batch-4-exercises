f4183beda96736439aff2168aa4062bd8cc2e15f
jithin tom <jithintom1@gmail.com>
2019-01-15 04:58:18 +0530

    reject promise & ispromise & promiseallprops

--- is-promise/isPromise.js
+++ is-promise/isPromise.js
@@ -1,6 +1,6 @@
 
-function isPromise(...args) {
-  return args;
+function isPromise(inputFunction) {
+  return  inputFunction instanceof Promise;
 }
 
 export {
--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,13 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promiseObject) {
+  const resultant = {}
+  promiseObject['foo'].then((result) => {
+    resultant['foo'] = result
+  });
+  promiseObject['bar'].then((result) => {
+    resultant['bar'] = result
+  });
+  return Promise.resolve(resultant)
 }
 
 export {
