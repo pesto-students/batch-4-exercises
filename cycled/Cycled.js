6e9465e25f4848ab8aff6073e81868b4a3e0a99d
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-16 00:08:41 +0530

    [WIP]: Complete day-7-segment-1

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,58 @@
 
-function cycled(...args) {
-  return args;
+
+class Cycled {
+  constructor(dataArray) {
+    this.list = dataArray;
+    this.count = 0;
+  }
+
+  set index(value) {
+    if (value < 0 && value > this.list.length) {
+      return this.count;
+    }
+    return value;
+  }
+
+  get index() {
+    return this.count;
+  }
+
+  [Symbol.iterator]() {
+    return this;
+  }
+
+  previous() {
+    if (this.count === 0) {
+      this.count = this.list.length - 1;
+    } else {
+      this.count -= 1;
+    }
+    return this.list[this.count];
+  }
+
+  step(takenStep) {
+    if (takenStep < 0) {
+      this.count = this.count + (takenStep % this.list.length);
+    } else {
+      this.count = this.count + (takenStep % this.list.length);
+    }
+    return this.list[this.count];
+  }
+
+  next() {
+    if (this.count + 1 === this.list.length) {
+      this.count = 0;
+    } else {
+      this.count += 1;
+    }
+    return this.list[this.count];
+  }
+
+  current() {
+    return this.list[this.count];
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
