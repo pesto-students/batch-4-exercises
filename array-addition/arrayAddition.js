ee4015dc5c130094f1521c8d35ccc9d8eab8e66f
jithin tom <jithintom1@gmail.com>
2019-01-16 04:09:06 +0530

    day-7-segment-2

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,26 @@
 
-function arrayAddition(...args) {
-  return args;
+function arrayAddition(array1, array2) {
+  let longerArray = [];
+  let shorterArray = [];
+  if ((array1.length === 0) || (array2.length === 0)) {
+    throw new Error('The lengths of the array does not match');
+  }
+  if (array2.length > array1.length) {
+    longerArray = array2;
+    shorterArray = array1;
+  } else {
+    longerArray = array1;
+    shorterArray = array2;
+  }
+  const resultant = [];
+  shorterArray.forEach((_element, index) => {
+    if ((typeof longerArray[index] !== 'number') || (typeof shorterArray[index] !== 'number')) {
+      throw new Error('Elements of the array are not numbers');
+    }
+    resultant.push(longerArray[index] + shorterArray[index]);
+  });
+  resultant.concat();
+  return resultant;
 }
 
 export {
