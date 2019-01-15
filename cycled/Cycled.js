6cd3e9fbb23adc32c0c82bc46e53bd05130f9dad
Prakhar Yadav <prakhary95@gmail.com>
2019-01-16 03:43:25 +0530

    WIP Day 7 Segment 1

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,25 @@
 
-function cycled(...args) {
-  return args;
+class Cycled {
+  constructor(arrayToCycle) {
+    this.cycleArray = arrayToCycle;
+    this.currentPosition = 0;
+    this.cycleArrayLength = arrayToCycle.length;
+  }
+
+  [Symbol.iterator]() {
+    return {
+      next() {
+        this.currentPosition++;
+        return this.cycleArray[this.currentPosition];
+      }
+    }
+  }
+
+  next() {
+    return this[Symbol.iterator].next();
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
