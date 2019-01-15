fc5a27d02193ab92f7e1a1b18202fbb131a2eed1
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 00:10:28 +0530

    wip

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,8 +1,15 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
-
-export {
-  fibonacciIter,
+const fibonacciIter = {
+  no1: 0,
+  no2: 1,
+  [Symbol.iterator]() {
+    return this;
+  },
+  next() {
+    const temp = this.no1 + this.no2;
+    this.no1 = this.no2;
+    this.no2 = temp;
+    return { value: temp };
+  },
 };
+
+export { fibonacciIter };
