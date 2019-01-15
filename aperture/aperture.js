9a5ab1e1fc02befac250a363cc9d6e80929cab7f
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 04:48:10 +0530

    update: implement aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,8 +1,10 @@
-
-function aperture(...args) {
-  return args;
+function aperture(n, array) {
+  if (array.length < n) return [];
+  const result = [];
+  for (let i = 0; i + n <= array.length; i += 1) {
+    result.push(array.slice(i, i + n));
+  }
+  return result;
 }
 
-export {
-  aperture,
-};
+export { aperture };
