f712e78ac64c16aa54c2b447fef3bc178ed8d19e
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 01:44:53 +0530

    update: implement generator-iterable

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,8 +1,12 @@
-
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable() {
+  let i = 1;
+  while (true) {
+    yield i;
+    if (i === 5) {
+      break;
+    }
+    i += 1;
+  }
 }
 
-export {
-  generatorIterable,
-};
+export { generatorIterable };
