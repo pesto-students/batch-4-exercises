90a10962de696c8737bed741575214fa387cb959
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 04:05:54 +0530

    wip

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,8 +1,26 @@
+// import { runInNewContext } from 'vm';
 
-function ackermann(...args) {
-  return args;
+const map = new Map();
+
+function ackermann(m, n) {
+  const key = `${m}-${n}`;
+  if (map.has(key)) {
+    return map.get(key);
+  }
+  if (m === 0) {
+    const val = n + 1;
+    map.set(key, val);
+    return val;
+  }
+  if (n === 0) {
+    const val = ackermann(m - 1, 1);
+    map.set(`${m - 1}-1`, val);
+    return val;
+  }
+  const val = ackermann(m, n - 1);
+  map.set(`${m}-${n - 1}`, val);
+  const nVal = val;
+  return ackermann(m - 1, nVal);
 }
 
-export {
-  ackermann,
-};
+export { ackermann };
