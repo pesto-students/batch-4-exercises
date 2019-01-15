5ed1442457176ba7d59d86e9a479210d9a9efb1c
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 04:35:08 +0530

    update: implement animal-sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,8 +1,7 @@
-
-function animalSort(...args) {
-  return args;
+function animalSort(array) {
+  return array.sort(
+    (obj1, obj2) => obj1.numberOfLegs - obj2.numberOfLegs || (obj1.name >= obj2.name ? 1 : -1),
+  );
 }
 
-export {
-  animalSort,
-};
+export { animalSort };
