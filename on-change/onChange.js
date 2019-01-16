224486b1f5321ba55e5f7f8d9fcefa145d934d59
Nitin Patel <nitinpatel278@gmail.com>
2019-01-17 00:13:06 +0530

    proxy-wip

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,8 +1,22 @@
+const getProxyHandler = callback => ({
+  set(obj, key, value) {
+    callback();
+    return Reflect.set(obj, key, value);
+  },
 
-function onChange(...args) {
-  return args;
+  defineProperty(obj, key, value) {
+    callback();
+    return Reflect.defineProperty(obj, key, value);
+  },
+
+  deleteProperty(obj, key) {
+    callback();
+    return Reflect.deleteProperty(obj, key);
+  },
+});
+
+function onChange(fixture, callback) {
+  return new Proxy({ ...fixture }, getProxyHandler(callback));
 }
 
-export {
-  onChange,
-};
+export { onChange };
