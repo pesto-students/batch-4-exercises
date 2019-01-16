d7239214d500d8306e33ded7d2a0f1a3894adf49
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-17 04:27:03 +0530

    update: implement splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,24 @@
-
-function splitEvery(...args) {
-  return args;
+function splitEvery(number, list) {
+  if (number < 1) {
+    throw new Error();
+  }
+  const isListOfTypeString = typeof list === 'string';
+  const result = [];
+  for (let i = 0; i < list.length; i += number) {
+    let innerResult = [];
+    if (isListOfTypeString) {
+      innerResult = '';
+    }
+    for (let j = i; j < list.length && j < i + number; j += 1) {
+      if (isListOfTypeString) {
+        innerResult += list.charAt(j);
+      } else {
+        innerResult.push(list[j]);
+      }
+    }
+    result.push(innerResult);
+  }
+  return result;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
