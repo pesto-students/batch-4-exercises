63ca65cc5024fef57f23542a41745c137e334ab4
Nitin Patel <nitinpatel278@gmail.com>
2019-01-17 04:40:05 +0530

    sundan

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,24 @@
+function sundanFn({ n, x, y }) {
+  if (n === 0) {
+    return x + y;
+  }
+  if (y === 0) {
+    return x;
+  }
 
-function sudanFn(...args) {
-  return args;
+  const newN = n - 1;
+  const newX = sundanFn({
+    n,
+    x,
+    y: y - 1,
+  });
+  const newY = newX + y;
+
+  return sundanFn({
+    n: newN,
+    x: newX,
+    y: newY,
+  });
 }
 
-export {
-  sudanFn,
-};
+export { sundanFn };
