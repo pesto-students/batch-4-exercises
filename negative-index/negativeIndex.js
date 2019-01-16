93591e12a4dbf034f77feee7ebf65e8680d58ae2
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-17 00:25:14 +0530

    wip: implement negativeIndex

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,8 +1,26 @@
-
-function negativeIndex(...args) {
-  return args;
+const not = val => !val;
+const isNumber = val => not(Number.isNaN(val));
+const isNegativeNumber = val => isNumber(val) && val < 0;
+function negativeIndex(argObject) {
+  if (not(Array.isArray(argObject))) {
+    throw new TypeError('Only arrays are supported');
+  }
+  return new Proxy(argObject, {
+    get(target, key) {
+      if (isNegativeNumber(key)) {
+        return target[target.length + (Number(key) % target.length)];
+      }
+      return target[key];
+    },
+    set(target, key, value) {
+      /* eslint-disable */
+      if (isNegativeNumber(key)) {
+        target[target.length + (Number(key) % target.length)] = value;
+      }
+      target[key] = value;
+      return true;
+    }
+  });
 }
 
-export {
-  negativeIndex,
-};
+export { negativeIndex };
