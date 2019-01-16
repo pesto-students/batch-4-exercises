4317b90869233dfa2e38c1c905662b449c8382d5
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-17 04:12:27 +0530

    [WIP]: Complete day 8 segment 2

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,20 @@
 
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix(matrixAndSearchValueObject) {
+  const { search, matrix } = matrixAndSearchValueObject;
+  // eslint-disable-next-line guard-for-in
+  for (const row of matrix) {
+    if (search < row[0]) {
+      return false;
+      // eslint-disable-next-line no-else-return
+    } else if (search > row[row.length - 1]) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    if (row.indexOf(search)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export {
