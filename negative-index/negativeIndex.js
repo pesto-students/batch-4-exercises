5ccfb7d428c9cbf44994f0fbb5323e70259b19ae
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-17 00:29:21 +0530

    Resolve onChange proxy test case failing

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,31 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(targetObj) {
+  if (!(targetObj instanceof Array)) {
+    throw new TypeError(/Only arrays are supported/);
+  }
+  const proxyHandler = {
+    get(target, key) {
+      if (key < 0) {
+        const rightIndex = target.length + parseInt(key, 10);
+        return Reflect.get(target, rightIndex);
+      }
+      return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+      console.log('key', key);
+      if (key < 0) {
+        console.log('key', key);
+        const rightIndex = target.length + parseInt(key, 10);
+        Reflect.set(target, rightIndex, value);
+        return true;
+      }
+      console.log('ehat', key);
+      Reflect.set(target, key, value);
+      return true;
+    },
+  };
+  const proxyObj = new Proxy(targetObj, proxyHandler);
+  return proxyObj;
 }
 
 export {
--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,30 @@
 
-function onChange(...args) {
-  return args;
+function onChange(fixture, changeCounterCallback) {
+  const proxyHandler = {
+    get(target, key) {
+      if (typeof target[key] === 'object' && target[key] !== null) {
+        return new Proxy(target[key], proxyHandler);
+      }
+      return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+      changeCounterCallback();
+      Reflect.set(target, key, value);
+      return true;
+    },
+    defineProperty(target, key, value) {
+      changeCounterCallback();
+      Object.defineProperty(target, key, value);
+      return true;
+    },
+    deleteProperty(target, key) {
+      changeCounterCallback();
+      Reflect.deleteProperty(target, key);
+      return true;
+    },
+  };
+  const proxyObj = new Proxy(fixture, proxyHandler);
+  return proxyObj;
 }
 
 export {
