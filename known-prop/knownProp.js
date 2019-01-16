0f15bcaec04d47e6ee8924e742538506e0085c6d
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-16 23:49:15 +0530

    update: implement knownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,12 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(targetObject) {
+  return new Proxy(targetObject, {
+    get(target, key) {
+      if (key in target) {
+        return target[key];
+      }
+      throw new TypeError('Unknown property');
+    },
+  });
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
