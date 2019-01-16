07d6414520df778d10d956e3ee92b60868ad0e22
jithin tom <jithintom1@gmail.com>
2019-01-16 23:57:23 +0530

    Day 8 segment 1

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,20 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(inputObject) {
+  const proxyObject = new Proxy(inputObject, {
+    getProperIndex(index) {
+      if (index < 0) {
+        return inputObject.length + Number(index);
+      }
+      return Number(index);
+    },
+    get(target, key) {
+      return inputObject[this.getProperIndex(key)];
+    },
+    set(target, key) {
+      Reflect.set(inputObject, this.getProperIndex(key));
+    },
+  });
+  return proxyObject;
 }
 
 export {
