69927e2d1cfc67a0c017f379b4a66e85ee4bca92
jithin tom <jithintom1@gmail.com>
2019-01-17 00:22:04 +0530

    Day 8 segment 1

--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,6 +1,17 @@
 
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(object, defaultValue) {
+  const proxyObject = new Proxy(object, {
+    get(target, key) {
+      if (key in target) {
+        return target[key];
+      }
+      return defaultValue;
+    },
+    set(target, key, value) {
+      Reflect.set(target, key, value);
+    },
+  });
+  return proxyObject;
 }
 
 export {
