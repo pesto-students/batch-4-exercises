268179d92c6be90772dcdfd0aeb98905b015e065
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-17 04:16:39 +0530

    update: implement slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,8 +1,29 @@
-
-function slice(...args) {
-  return args;
+function slice(fromIndex, toIndex, list) {
+  let startIndex = fromIndex;
+  let endIndex = toIndex;
+  if (fromIndex === -Infinity) {
+    startIndex = 0;
+  } else if (fromIndex < 0) {
+    startIndex = list.length + fromIndex;
+  }
+  if (toIndex === Infinity) {
+    endIndex = list.length;
+  } else if (toIndex < 0) {
+    endIndex = list.length + toIndex;
+  }
+  let result = [];
+  const isListOfTypeString = typeof list === 'string';
+  if (isListOfTypeString) {
+    result = '';
+  }
+  for (let i = startIndex; i < endIndex; i += 1) {
+    if (isListOfTypeString) {
+      result += list.charAt(i);
+    } else {
+      result.push(list[i]);
+    }
+  }
+  return result;
 }
 
-export {
-  slice,
-};
+export { slice };
