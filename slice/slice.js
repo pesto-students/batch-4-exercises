96e84156ba32cdb66a003853322784c0831f4792
jithin tom <jithintom1@gmail.com>
2019-01-17 04:03:41 +0530

    day-8-segment-2

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,15 @@
 
-function slice(...args) {
-  return args;
+function slice(start, end, object) {
+  const normalizeIndex = (index, target) => {
+    if (index > target.length) {
+      return target.length;
+    }
+    if (index < 0) {
+      return Math.abs(index) > target.length ? 0 : target.length + Number(index);
+    }
+    return Number(index);
+  };
+  return object.slice(normalizeIndex(start, object), normalizeIndex(end, object));
 }
 
 export {
