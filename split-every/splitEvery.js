f8f31f9a33a80f43c7e35adc8c92e66d5606223b
Nitin Patel <nitinpatel278@gmail.com>
2019-01-17 04:08:34 +0530

    split every

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,20 @@
+function splitEvery(chunkLength, iterable) {
+  if (chunkLength <= 0) {
+    throw new Error('Chunk length expected to be positive');
+  }
 
-function splitEvery(...args) {
-  return args;
+  const splitedArray = Array.from(iterable).reduce((accumulator, current) => {
+    if (accumulator.length === 0 || accumulator[accumulator.length - 1].length === chunkLength) {
+      return [...accumulator, [current]];
+    }
+    accumulator[accumulator.length - 1].push(current);
+    return accumulator;
+  }, []);
+
+  if (typeof iterable === 'string') {
+    return splitedArray.map(chunk => chunk.join(''));
+  }
+  return splitedArray;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
