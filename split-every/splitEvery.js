f6a6199c1804ad70e93c0811dfb239242c704939
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-17 04:27:23 +0530

    Resolve splitEvery test cases

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,18 @@
-
-function splitEvery(...args) {
-  return args;
+const isNotIterable = obj => !(typeof obj === 'string' || Array.isArray(obj));
+function splitEvery(splitLength, iterableObj) {
+  if (isNotIterable(iterableObj) || splitLength < 1) {
+    throw new Error();
+  }
+  let currentSplitIndex = 0;
+  const splittedArray = [];
+  while (currentSplitIndex + splitLength < iterableObj.length) {
+    splittedArray.push(iterableObj.slice(currentSplitIndex, currentSplitIndex + splitLength));
+    currentSplitIndex += splitLength;
+  }
+  if (currentSplitIndex < iterableObj.length) {
+    splittedArray.push(iterableObj.slice(currentSplitIndex));
+  }
+  return splittedArray;
 }
 
 export {
