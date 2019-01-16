f25147427eed07c91c3443f6402677747cdad6a9
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-16 23:51:00 +0530

    Complete day 8 segment 1

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,14 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(targetObj) {
+  const proxyHandler = {
+    get(target, key) {
+      if (!Object.prototype.hasOwnProperty.call(target, key)) {
+        throw new TypeError(/Unknown property/);
+      }
+      return Reflect.get(target, key);
+    },
+  };
+  const proxyObj = new Proxy(targetObj, proxyHandler);
+  return proxyObj;
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
