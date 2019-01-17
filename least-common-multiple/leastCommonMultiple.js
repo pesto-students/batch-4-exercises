cc5bcd8fbadfac2cf6ffa411b91103ec8cc48bf6
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-18 04:52:53 +0530

    [WIP]: day 9 segment 2

--- least-common-multiple/leastCommonMultiple.js
+++ least-common-multiple/leastCommonMultiple.js
@@ -1,6 +1,18 @@
 
-function leastCommonMultiple(...args) {
-  return args;
+function leastCommonMultiple(firstNumber, secondNumber) {
+  let greaterNumber;
+  if (firstNumber >= secondNumber) {
+    greaterNumber = Math.abs(firstNumber);
+  }
+  else {
+    greaterNumber = Math.abs(secondNumber);
+  }
+  while (!(greaterNumber % firstNumber === 0 && greaterNumber % secondNumber === 0)) {
+    greaterNumber += 1;
+  }
+  const lcm = greaterNumber;
+
+  return lcm;
 }
 
 export {
