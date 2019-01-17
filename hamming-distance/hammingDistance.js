d60bfa68f3eb0e2eb7afea3590f080238e5f0adc
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-18 00:34:04 +0530

    Resolve html issues and hammingDistance problem

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,14 @@
-
-function hammingDistance(...args) {
-  return args;
+const isEqualLength = (obj1, obj2) => obj1.length === obj2.length;
+function hammingDistance(string1, string2) {
+  if (!isEqualLength(string1, string2)) {
+    throw new Error();
+  }
+  return string1.split('').reduce((hammingCounter, element, index) => {
+    if (element !== string2[index]) {
+      return hammingCounter + 1;
+    }
+    return hammingCounter;
+  }, 0);
 }
 
 export {
