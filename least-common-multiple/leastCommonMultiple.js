a09ddb6c3921ed668622c9381c60d6e68b12d3ac
Nitin Patel <nitinpatel278@gmail.com>
2019-01-18 04:42:58 +0530

    wip-commit

--- least-common-multiple/leastCommonMultiple.js
+++ least-common-multiple/leastCommonMultiple.js
@@ -1,8 +1,20 @@
+const isLCM = (guess, num1, num2) => guess % num1 === 0 && guess % num2 === 0;
 
-function leastCommonMultiple(...args) {
-  return args;
+function leastCommonMultiple(num1, num2) {
+  // find larger of two numbers, remove negative signs if any
+  const [smaller, bigger] = [Math.abs(num1), Math.abs(num2)].sort();
+
+  const smallestPossibleLCM = bigger;
+
+  const biggestPossibleLCM = smaller * bigger;
+
+  for (let guess = smallestPossibleLCM; guess <= biggestPossibleLCM; guess += 1) {
+    if (isLCM(guess, num1, num2)) {
+      return guess;
+    }
+  }
+
+  return biggestPossibleLCM;
 }
 
-export {
-  leastCommonMultiple,
-};
+export { leastCommonMultiple };
