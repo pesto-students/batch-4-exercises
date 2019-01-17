a1006d5316376630c70237adc061196e73376d7a
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-18 01:35:55 +0530

    Added Head and Meta data to html files.

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,6 +1,19 @@
 
-function binarySearch(...args) {
-  return args;
+// eslint-disable-next-line consistent-return
+function binarySearch(arr, searchKey) {
+  if (typeof searchKey === 'number') {
+    const midIndex = Math.round(arr.length / 2) - 1;
+    const mediumNumberOfArray = arr[midIndex];
+    if (arr.length === 0) {
+      return 0;
+    }
+    if (mediumNumberOfArray === searchKey) {
+      return mediumNumberOfArray;
+    }
+    if (searchKey >= mediumNumberOfArray) {
+      binarySearch(arr.slice(midIndex + 1));
+    }
+  }
 }
 
 export {
