9535b41656591065da2acf52e09679c4043f7487
jithin tom <jithintom1@gmail.com>
2019-01-18 04:13:05 +0530

    day-9-segment-1

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,6 +1,21 @@
 
-function binarySearch(...args) {
-  return args;
+function binarySearch(array, elementToBeSearched) {
+  let start = 0;
+  let end = array.length - 1;
+  let mid = (start + end) / 2;
+  while (start < end) {
+    if (array[mid] === elementToBeSearched) {
+      return mid;
+    }
+    if (elementToBeSearched > array[mid]) {
+      start = mid;
+    }
+    if (elementToBeSearched < array[mid]) {
+      end = mid;
+    }
+    mid = (start + end) / 2;
+  }
+  return -1;
 }
 
 export {
--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,22 @@
-
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if ((number === 1) || (number === -1)) {
+    return false;
+  }
+  if (number === 0) {
+    return false;
+  }
+  if (number === 2) {
+    return true;
+  }
+  const sqrt = Math.ceil(Math.sqrt(number));
+  let start = 2;
+  while (start <= sqrt) {
+    if (number % start === 0) {
+      return false;
+    }
+    start += 1;
+  }
+  return true;
 }
 
 export {
