34432b7ce1e7e63f9b73f3bcae93a45a4b344ae6
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-17 23:59:26 +0530

    [WIP]: day 9 segment 1

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,6 +1,19 @@
+const isPresentInArray = (start, end, array, number) => {
+  for (let i = start; i < end; i += 1) {
+    if (array[i] === number) {
+      return i;
+    }
+  }
+  return -1;
+};
 
-function binarySearch(...args) {
-  return args;
+function binarySearch(numbers, numberToSearch) {
+  const halfLength = parseInt(numbers.length / 2, 10);
+  const middleNumber = numbers[halfLength];
+  if (numberToSearch <= middleNumber) {
+    return isPresentInArray(0, middleNumber, numbers, numberToSearch);
+  }
+  return isPresentInArray(halfLength, numbers.length, numbers, numberToSearch);
 }
 
 export {
