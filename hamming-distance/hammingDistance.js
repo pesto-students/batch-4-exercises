ee2c5ef71fef34cf27a69aca7246decba547c37d
Nitin Patel <nitinpatel278@gmail.com>
2019-01-17 23:49:43 +0530

    wip-commit

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,8 +1,16 @@
+const lengthsDiffer = (wordOne, wordTwo) => wordOne.length !== wordTwo.length;
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(wordOne, wordTwo) {
+  if (lengthsDiffer(wordOne, wordTwo)) {
+    throw new Error('Words should be same length');
+  }
+
+  return Array.from(wordOne).reduce((accumulator, currentChar, index) => {
+    if (currentChar !== wordTwo[index]) {
+      return accumulator + 1;
+    }
+    return accumulator;
+  }, 0);
 }
 
-export {
-  hammingDistance,
-};
+export { hammingDistance };
