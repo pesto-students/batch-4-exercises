cf0d5e3d1ae424d477c84e504c469e12ba3329f6
Nitin Patel <nitinpatel278@gmail.com>
2019-01-18 00:21:39 +0530

    wip-commit

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,8 +1,20 @@
+const isDivisible = (number, divisor) => number % divisor === 0;
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  const smallestDivisorToCheck = 2;
+  const largestDivisorToCheck = Math.floor(Math.sqrt(number));
+
+  for (let divisor = smallestDivisorToCheck; divisor <= largestDivisorToCheck; divisor += 1) {
+    if (isDivisible(number, divisor)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-export {
-  primalityTest,
-};
+export { primalityTest };
