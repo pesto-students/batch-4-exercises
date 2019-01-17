8b4221f1520b4cba20a656e2d7c7f70deb52b4b9
Nitin Patel <nitinpatel278@gmail.com>
2019-01-18 00:17:43 +0530

    wip-commit

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,8 +1,41 @@
+const compare = (elementInMiddle, target, comparator) => {
+  if (comparator) {
+    return comparator(elementInMiddle, target);
+  }
+  return elementInMiddle - target;
+};
 
-function binarySearch(...args) {
-  return args;
+function binarySearchNumbers(sortedData, start, end, target, comparator) {
+  if (end - start < 0) {
+    return -1;
+  }
+  const mid = Math.floor((start + end) / 2);
+  const elementInMiddle = sortedData[mid];
+
+  const comparisonResult = compare(elementInMiddle, target, comparator);
+
+  if (comparisonResult === 0) {
+    return mid;
+  }
+
+  if (comparisonResult > 0) {
+    return binarySearchNumbers(sortedData, start, mid - 1, target, comparator);
+  }
+
+  if (comparisonResult < 0) {
+    return binarySearchNumbers(sortedData, mid + 1, end, target, comparator);
+  }
+
+  return -1;
 }
 
-export {
-  binarySearch,
-};
+function binarySearch(sortedData, target, comparator) {
+  // eslint-disable-next-line prefer-destructuring
+  const length = sortedData.length;
+
+  const start = 0;
+  const end = length - 1;
+  return binarySearchNumbers(sortedData, start, end, target, comparator);
+}
+
+export { binarySearch };
