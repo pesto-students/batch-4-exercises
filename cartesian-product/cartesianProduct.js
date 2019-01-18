5ca7d2ec91da05bcb5cd435797e424b91a7f2b15
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-18 23:55:59 +0530

    [WIP]: day 10 segment 1

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,6 +1,15 @@
 
-function cartesianProduct(...args) {
-  return args;
+function cartesianProduct(firstArray, secondArray) {
+  if (firstArray === null || secondArray === null) {
+    return null;
+  }
+  if (firstArray.length === 0 || secondArray.length === 0) {
+    return null;
+  }
+  return firstArray.reduce((acc, curVal) => {
+    const row = secondArray.map(value => [curVal, value]);
+    return [...acc, ...row];
+  }, []);
 }
 
 export {
