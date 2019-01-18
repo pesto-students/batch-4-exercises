a594b8e4ba89b929a40808443f5fae1174462169
Nitin Patel <nitinpatel278@gmail.com>
2019-01-19 00:19:57 +0530

    WIPcommit

--- cornered-string/corneredString.js
+++ cornered-string/corneredString.js
@@ -1,8 +1,14 @@
+const capitalizeAndJoinWith = joiner => letterArray => letterArray
+  .map(letter => letter.toUpperCase())
+  .join(joiner);
 
-function corneredString(...args) {
-  return args;
+function corneredString(word) {
+  const lettersInFirstLine = Array.from(word);
+  // remove first character because it is already printed in first line;
+  const lettersInNextLines = Array.from(word).slice(1);
+  const firstLine = capitalizeAndJoinWith(' ')(lettersInFirstLine);
+  const nextLines = capitalizeAndJoinWith('\n\n')(lettersInNextLines);
+  return `${firstLine}\n\n${nextLines}`.trim();
 }
 
-export {
-  corneredString,
-};
+export { corneredString };
