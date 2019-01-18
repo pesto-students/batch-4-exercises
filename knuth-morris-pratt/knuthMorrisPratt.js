a74f1a428deef7e530c044441b997decfff5703b
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-18 18:56:33 +0530

    Resolve knuthMorrisPratt problem

--- knuth-morris-pratt/knuthMorrisPratt.js
+++ knuth-morris-pratt/knuthMorrisPratt.js
@@ -1,8 +1,16 @@
-
-function knuthMorrisPratt(...args) {
-  return args;
+/* eslint-disable guard-for-in */
+function knuthMorrisPratt(string1, subString) {
+  const subStringLength = subString.length;
+  for (const index in string1) {
+    const parsedIntegerIndex = parseInt(index, 10);
+    const offset = parsedIntegerIndex + subStringLength;
+    if (offset > string1.length) {
+      return -1;
+    }
+    if (string1.slice(index, offset) === subString) {
+      return parsedIntegerIndex;
+    }
+  }
+  return -1;
 }
-
-export {
-  knuthMorrisPratt,
-};
+export { knuthMorrisPratt };
