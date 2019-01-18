440f16ce3dd7df90a452589f4bcf54e870614d7d
Nitin Patel <nitinpatel278@gmail.com>
2019-01-19 00:03:39 +0530

    WIPcommit

--- center-text/centerText.js
+++ center-text/centerText.js
@@ -1,8 +1,17 @@
+/* This problem is not done yet, will come back to it later. */
 
-function centerText(...args) {
-  return args;
+const splitIntoChunksOf = (sentence, chunkLength) => sentence.split(' ').reduce((accumulator, currentWord) => {
+  if (accumulator.length === 0 || accumulator[accumulator.length - 1].length >= chunkLength - 1) {
+    return [...accumulator, ` ${currentWord}`];
+  }
+  const lastWord = accumulator.pop();
+  const newLastWord = `${lastWord} ${currentWord}`;
+  return [...accumulator, newLastWord];
+}, []);
+
+function centerText(sentence, chunkLength) {
+  const splitedSentence = splitIntoChunksOf(sentence, chunkLength);
+  return splitedSentence;
 }
 
-export {
-  centerText,
-};
+export { centerText };
