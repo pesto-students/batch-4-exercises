c172d2a2bf2a43bc1ab9cb232d75d9a49cb1e847
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-18 19:25:32 +0530

    Resolve test of primality test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,8 +1,17 @@
-
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if (number <= 1) {
+    return 0;
+  }
+  if (number === 2) {
+    return 1;
+  }
+  const roundedSquareRoot = Math.round(Math.sqrt(number));
+  for (let x = 2; x <= roundedSquareRoot; x += 1) {
+    if (number % x === 0) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
-export {
-  primalityTest,
-};
+export { primalityTest };
