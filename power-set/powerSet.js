e4ed73fb687c5c6cbc22ea4ff759b23772905f0f
Nitin Patel <nitinpatel278@gmail.com>
2019-01-20 07:01:51 +0530

    WIPcommit

--- power-set/powerSet.js
+++ power-set/powerSet.js
@@ -1,24 +1,8 @@
 function powerSet(numberSet) {
-  if (numberSet.length === 0) {
-    return [[]];
-  }
-
-  if (numberSet.length === 1) {
-    return [[], numberSet];
-  }
-
-  const powerSetOfFirstElement = powerSet(numberSet.slice(0, 1));
-
-  const powerSetOfRestElements = numberSet
-    .slice(1)
-    .reduce(
-      (accumulator, current) => [...accumulator, ...accumulator.map(set => [...set, current])],
-      powerSetOfFirstElement,
-    );
-
-  const fullPowerSet = [...powerSetOfFirstElement, ...powerSetOfRestElements];
-
-  return fullPowerSet;
+  return numberSet.reduce(
+    (accumulator, current) => [...accumulator, ...accumulator.map(set => [...set, current])],
+    [[]],
+  );
 }
 
 export { powerSet };
