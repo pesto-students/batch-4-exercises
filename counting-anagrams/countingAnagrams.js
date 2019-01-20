8373c73d2558e41140c3f5b22d3a99d1720bc41c
Nitin Patel <nitinpatel278@gmail.com>
2019-01-20 23:14:17 +0530

    WIPcommit

--- counting-anagrams/countingAnagrams.js
+++ counting-anagrams/countingAnagrams.js
@@ -1,8 +1,24 @@
+function countingAnagrams(wordList) {
+  const wordArray = wordList.split(' ');
 
-function countingAnagrams(...args) {
-  return args;
+  const sortedWordCount = wordArray.reduce((counterObject, word) => {
+    const sortedWord = word
+      .split('')
+      .sort()
+      .join('');
+
+    if (Object.keys(counterObject).includes(sortedWord)) {
+      return { ...counterObject, [sortedWord]: counterObject[sortedWord] + 1 };
+    }
+    return { ...counterObject, [sortedWord]: 1 };
+  }, {});
+
+  return Object.entries(sortedWordCount).reduce((anagramCount, [word, frequency]) => {
+    if (word.length > 1 && frequency > 1) {
+      return anagramCount + 1;
+    }
+    return anagramCount;
+  }, 0);
 }
 
-export {
-  countingAnagrams,
-};
+export { countingAnagrams };
