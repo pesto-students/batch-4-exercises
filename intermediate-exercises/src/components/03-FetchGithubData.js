64e7551f5f66576bf8903e09db3309c0293e767f
Nitin Patel <nitinpatel278@gmail.com>
2019-01-23 04:59:35 +0530

    github fetch

--- intermediate-exercises/src/components/03-FetchGithubData.js
+++ intermediate-exercises/src/components/03-FetchGithubData.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import axios from 'axios';
+import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
+import axios from 'axios';
 
-/**
- * Axios is a promise based HTTP client for the browser and node.js.
- * Refer to their github page to see how to use it to make HTTP requests.
- * Axios: https://github.com/axios/axios
- *
- * It is best not to fetch data from a server in the `render` method.
- * Any change to the state of a component can cause a re-render of the
- * component. This will likely happen more often than we want.
- * Use the appropriate lifecycle method to make the axios request.
- *
- * Exercise:
- *
- *  Create a `GithubRepos` component that lists all the GitHub repos for a user.
- *  Allow the repos to be provided as a prop.
- *
- *  https://api.github.com/users/{username}/repos
- */
 /* eslint-disable react/no-unused-state */
 const GithubRepos = ({ repos }) => {
   return (
     <ul>
-      {/* Task: The list of repos here */}
+      {repos.map(repo => (
+        <RepoItem repo={repo} key={repo.name} />
+      ))}
     </ul>
   );
-}
+};
 
-// Task: Open the console in the browser. There will be a warning
-// about incorrect prop type for user.
-// Define the correct prop type for the prop `repos`
 GithubRepos.propTypes = {
-
+  repos: PropTypes.array.isRequired,
 };
 
 /* eslint-disable react/no-multi-comp */
@@ -44,20 +27,33 @@ class UsernameForm extends Component {
       repos: [],
     };
   }
+
+  fetchReposAndUpdate = () => {
+    const { username } = this.state;
+    const apiEndPoint = `https://api.github.com/users/${username}/repos`;
+
+    axios
+      .get(apiEndPoint)
+      .then(response => response.data)
+      .then(repos => {
+        this.setState({ repos });
+      });
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { repos } = this.state;
     return (
       <div>
-        <input
-          type="text"
-          name="username"
-        />
-        <button
-          onClick={() => {}}
-        >
-          Get Repos
-        </button>
-        {/* Task: Display the results here. Use GithubRepos Component.
-          It should be a list of repos of the user entered */}
+        <input type="text" name="username" onChange={this.handleInputChange} />
+        <button onClick={this.fetchReposAndUpdate}>Get Repos</button>
+
+        <GithubRepos repos={repos} />
       </div>
     );
   }
---
+++ intermediate-exercises/src/components/RepoItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RepoInfo = ({ repo }) => <li>{repo.name}</li>;
+
+export default RepoInfo;
