cea1f5987a7425897188ea147b373c18cb7789ff
Nitin Patel <nitinpatel278@gmail.com>
2019-01-23 05:14:12 +0530

    proptypes to repoitem

--- intermediate-exercises/src/components/RepoItem.js
+++ intermediate-exercises/src/components/RepoItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const RepoInfo = ({ repo }) => <li>{repo.name}</li>;
+
+RepoInfo.propTypes = {
+  repo: PropTypes.object.isRequired,
+};
 
 export default RepoInfo;
