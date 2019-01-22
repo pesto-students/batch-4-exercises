59fdd7b33b6037b3feb98951606edc12c32dff47
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-23 00:52:57 +0530

    Complete Isomorphic.js

--- isomorphic/isomorphic.js
+++ isomorphic/isomorphic.js
@@ -1,8 +1,20 @@
-
-function isomorphic(...args) {
-  return args;
+function isomorphic([firstWord, secondWord]) {
+  if (firstWord.length !== secondWord.length) {
+    return false;
+  }
+  const letterMap = new Map();
+  for (let i = 0; i < firstWord.length; i += 1) {
+    const charFirstWord = firstWord.charAt(i);
+    if (!letterMap.has(charFirstWord)) {
+      letterMap.set(charFirstWord, secondWord.charAt(i));
+    } else {
+      const temp = letterMap.get(charFirstWord);
+      if (secondWord.charAt(i) !== temp) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-export {
-  isomorphic,
-};
+export { isomorphic };
