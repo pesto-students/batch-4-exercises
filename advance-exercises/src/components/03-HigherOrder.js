d6032083bcf63bf2403b76d878069cc48d41a615
Nitin Patel <nitinpatel278@gmail.com>
2019-01-24 00:14:15 +0530

    hoc

--- advance-exercises/src/components/03-HigherOrder.js
+++ advance-exercises/src/components/03-HigherOrder.js
@@ -9,7 +9,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function withMouse(Component) {
-  return Component;
+  return class WithMouse extends React.Component {
+    state = {
+      mouse: null,
+    };
+
+    updateMousePosition = (event) => {
+      this.setState({
+        mouse: {
+          x: event.clientX,
+          y: event.clientY,
+        },
+      });
+    };
+
+    render() {
+      return (
+        <div onMouseMove={this.updateMousePosition}>
+          <Component mouse={this.state.mouse} />
+        </div>
+      );
+    }
+  };
 }
 
 class App extends React.Component {
@@ -20,7 +41,12 @@ class App extends React.Component {
       <div className="container">
         {mouse ? (
           <h1>
-            The mouse position is ({mouse.x}, {mouse.y})
+            The mouse position is (
+            {mouse.x}
+,
+            {' '}
+            {mouse.y}
+)
           </h1>
         ) : (
           <h1>We don&#39;t know the mouse position yet :(</h1>
@@ -34,7 +60,11 @@ App.propTypes = {
   mouse: PropTypes.shape({
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired,
-  }).isRequired,
+  }),
+};
+
+App.defaultProps = {
+  mouse: null,
 };
 
 const AppWithMouse = withMouse(App);
