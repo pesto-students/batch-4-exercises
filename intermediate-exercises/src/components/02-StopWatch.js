9d3b710ad3826133a551d4a8d3081a47ed1af3e4
Prakhar Yadav <prakhary95@gmail.com>
2019-01-23 17:05:13 +0530

    StopWatch WIP

--- intermediate-exercises/src/components/02-StopWatch.js
+++ intermediate-exercises/src/components/02-StopWatch.js
@@ -17,10 +17,83 @@ import React, { Component } from 'react';
 *  Clicking Clear will stop the timer if it's running and reset the lapsed time to 0.
 */
 
-class StopWatch extends Component {
+class TimeDisplay extends Component {
   render() {
     return (
-      <div>Stop Watch</div>
+      <p>{ `${this.props.minutes} : ${this.props.seconds}` }</p>
+    );
+  }
+}
+
+class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+    this.getTimeInMinutesAndSeconds = this.getTimeInMinutesAndSeconds.bind(this);
+    this.addLeadingZero = this.addLeadingZero.bind(this);
+    this.startOrStop = this.startOrStop.bind(this);
+    this.state = {
+      seconds: 0,
+      running: false,
+      interval: null,
+    };
+  }
+
+  addLeadingZero(number) {
+    if (number < 10) {
+      return `0${number}`;
+    }
+    return `${number}`;
+  }
+
+  getTimeInMinutesAndSeconds() {
+    const secondsElapsed = this.state.seconds;
+    const minutes = parseInt(secondsElapsed / 60, 10);
+    const seconds = secondsElapsed % 60;
+
+    return {
+      minutes: this.addLeadingZero(minutes),
+      seconds: this.addLeadingZero(seconds),
+    }
+  }
+
+  startOrStop() {
+    let seconds = this.state.seconds;
+    let running = this.state.running;
+
+    if (this.state.running) {
+      clearInterval(this.state.interval);
+    } else {
+
+      const interval = setInterval(() => {
+        seconds += 1;
+        this.setState({
+          ...this.state,
+          seconds,
+        })
+      },1000);
+
+      this.setState({
+        ...this.state,
+        interval: interval,
+      });
+    }
+
+    running = !running;
+    this.setState({
+      ...this.state,
+      running,
+    });
+  }
+  
+  render() {
+    const { minutes, seconds } = this.getTimeInMinutesAndSeconds();
+    const buttonText = this.state.running ? 'Stop' : 'Start';
+    return (
+      <div className="stop-watch-container">
+        <h1>Stop Watch</h1>
+        <TimeDisplay minutes={minutes} seconds={seconds}></TimeDisplay>
+        <button id="start-stop" onClick={this.startOrStop}>{ buttonText }</button>
+      </div>
     );
   }
 }
