90f097b95d6df0e4ee622e2ed757fc04e823c9a9
Nitin Patel <nitinpatel278@gmail.com>
2019-01-25 05:21:28 +0530

    prompt when changes not saved

---
+++ colorful-router-2/src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './styles/App.css';
+import ColorList from './components/ColorList';
+import Color from './components/Color';
+import NewColor from './components/NewColor';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: [
+        {
+          name: 'red',
+          hex: '#FF0000',
+        },
+        {
+          name: 'green',
+          hex: '#00FF00',
+        },
+        {
+          name: 'blue',
+          hex: '#0000FF',
+        },
+      ],
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(newColor) {
+    this.setState({ colors: [newColor, ...this.state.colors] });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/colors">
+          <ColorList colors={this.state.colors} handleAdd={this.handleAdd} />
+        </Route>
+
+        <Route
+          path="/colors/new"
+          render={props => <NewColor addColor={this.handleAdd} {...props} />}
+        />
+
+        {this.state.colors.map((color) => {
+          return (
+            <Route
+              path={`/colors/${color.name}`}
+              key={color.name}
+              render={(props) => {
+                return <Color color={color} {...props} />;
+              }}
+            />
+          );
+        })}
+      </Switch>
+    );
+  }
+}
+
+export default App;
---
+++ colorful-router-2/src/components/NewColor.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Prompt } from 'react-router-dom';
+
+import '../styles/NewColor.css';
+
+class NewColor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      hex: '#ffffff',
+      changesSaved: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {}
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addColor({ ...this.state });
+    this.setState(
+      {
+        changesSaved: true,
+      },
+      () => this.props.history.push('/colors'),
+    );
+  }
+
+  render() {
+    return (
+      <div className="new-color">
+        <Prompt when={!this.changesSaved} message="Are you sure you want to leave this page?" />
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name">Color name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter a name for the color"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+          </div>
+          <div>
+            <label htmlFor="hex">Color value</label>
+            <input
+              type="color"
+              name="hex"
+              id="hex"
+              onChange={this.handleChange}
+              value={this.state.hex}
+            />
+          </div>
+          <input type="Submit" value="Add this color" readOnly />
+        </form>
+      </div>
+    );
+  }
+}
+
+NewColor.propTypes = {
+  addColor: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired,
+};
+
+export default NewColor;
