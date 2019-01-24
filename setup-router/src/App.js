9f7ec2aff0352c3d9bf8e71777993f45dbc186a3
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-25 00:16:16 +0530

    Resolve react router problem

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
+import NotFound from './components/notFound'
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Router>
+          <div>
+            <Navbar></Navbar>
+            <Switch>
+              <Route exact path='/' component={Home}></Route>
+              <Route exact path='/about' component={About}></Route>
+              <Route exact path='/contact' component={Contact}></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
+          </div>
+        </Router>
       </Fragment>
     );
   }
