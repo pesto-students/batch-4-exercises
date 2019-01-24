7a1645d061e4d783ece33fba1d41102434e77e8f
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-25 00:42:13 +0530

    complete exercise omit

--- omit/omit.js
+++ omit/omit.js
@@ -1,8 +1,17 @@
-
-function omit(...args) {
-  return args;
+function omit(toDeletePropertyKeys, sourceObject) {
+  const sourcePrototypeObject = Object.getPrototypeOf(sourceObject);
+  const newObject = {};
+  Object.keys(sourceObject).forEach((key) => {
+    if (!toDeletePropertyKeys.includes(key)) {
+      newObject[key] = sourceObject[key];
+    }
+  });
+  Object.keys(sourcePrototypeObject).forEach((key) => {
+    if (!toDeletePropertyKeys.includes(key)) {
+      newObject[key] = sourceObject[key];
+    }
+  });
+  return newObject;
 }
 
-export {
-  omit,
-};
+export { omit };
