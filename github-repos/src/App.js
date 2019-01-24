615e93c1a4001b117523e76a3ced443a1844f019
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-24 05:31:31 +0530

    fix: github-repos

--- github-repos/src/App.js
+++ github-repos/src/App.js
@@ -7,26 +7,43 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
-      repoList: []
+      username: "",
+      repos: []
     };
   }
+  handleInputChange(evt) {
+    const username = evt.target.value;
+    this.setState(() => ({ username }));
+  }
   handleSubmit(evt) {
-    async () => {
-      await fetch;
-    };
+    evt.preventDefault();
+    (async () => {
+      const repoList = await fetch(
+        `https://api.github.com/users/${this.state.username}/repos`
+      ).then(res => res.json());
+      const repos = repoList.map(repo => repo.name);
+      this.setState(() => ({ repos }));
+    })();
   }
   render() {
     return (
       <Fragment>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Username : <input name="username" type="text" />
+            Username :{" "}
+            <input
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
           </label>
           <button>Submit</button>
         </form>
-        {this.state.repoList.map(repoName => (
-          <div>repoName</div>
+        {this.state.repos.map(repoName => (
+          <div>{repoName}</div>
         ))}
       </Fragment>
     );
