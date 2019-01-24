4ad5afe34fe9ca9eb76a04642448f9899ad727a3
Prakhar Yadav <prakhary95@gmail.com>
2019-01-25 04:59:35 +0530

    Implement Colorful Router

--- colorful-router/src/App.js
+++ colorful-router/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-// import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './styles/App.css';
 import ColorList from './components/ColorList';
-// import Color from './components/Color';
-// import NewColor from './components/NewColor';
+import Color from './components/Color';
+import NewColor from './components/NewColor';
+
+const redirectToURL = (url) => () => (<Redirect to={url} />);
 
 class App extends Component {
   constructor(props) {
@@ -36,9 +38,24 @@ class App extends Component {
     const colorListComponent = () => (
       <ColorList colors={this.state.colors} />
     );
+    const newColorComponent = (newColorProps) => {
+      const { history } = newColorProps;
+      return (<NewColor {...newColorProps} addColor={this.handleAdd} history={ history } />);
+    };
+    const colorComponent = (colorProps) => {
+      const { match: {params: { color } } } = colorProps;
+      const [ colorObject ] = this.state.colors.filter(el => el.name === color);
 
+      return colorObject ? <Color {...colorProps} color={colorObject} /> : <Redirect to="/" />;
+    };
     return (
-      colorListComponent()
+      <Switch>
+        <Redirect exact from="/" to="/colors" />
+        <Route exact path="/colors" render={colorListComponent} />
+        <Route path="/colors/new" render={newColorComponent} />
+        <Route path="/colors/:color" render={colorComponent} />
+        <Route render={redirectToURL('/')} />
+      </Switch>
     );
   }
 }
