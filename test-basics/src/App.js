6686ba3a118f107bfa0fbae0fc4195b2955b6af5
Prakhar Yadav <prakhary95@gmail.com>
2019-01-25 23:21:09 +0530

    Implement test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -4,15 +4,44 @@ import './styles/App.css';
 
 /* eslint-disable react/no-multi-comp, react/no-unused-state */
 class App extends Component {
-  state = {
-    on: false,
-    input: '',
-    mainColor: 'blue',
+  constructor(props) {
+    super(props);
+    this.changeText = this.changeText.bind(this);
+    this.updateHeading = this.updateHeading.bind(this);
+    this.state = {
+      on: false,
+      input: '',
+      mainColor: 'blue',
+    };
   }
+
+  changeText() {
+    this.setState(({ on }) => ({ on: !on }));
+  }
+
+  updateHeading(event) {
+    this.setState({ input: event.currentTarget.value });
+  }
+
+  handleStrings(stringToEvaluate) {
+    return Boolean(stringToEvaluate.length);
+  }
+
   render() {
     return (
-      <div className="App">
-        Welcome to React
+      <div className={this.state.mainColor}>
+        <h1 className="heading">Welcome to React</h1>
+        <h2 className="subheading">{this.state.input}</h2>
+
+        <button className="btn btn-primary" onClick={this.changeText}>
+          Change Text
+        </button>
+        <p className="button-state">{this.state.on ? 'Yes!' : 'No!'}</p>
+
+        <label htmlFor="heading-input">Change subheading</label>
+        <div className="field-container">
+          <input id="heading-input" name="heading-input" value={this.state.input} onChange={this.updateHeading} />
+        </div>
       </div>
     );
   }
@@ -20,7 +49,10 @@ class App extends Component {
 
 export class Link extends Component {
   render() {
-    return null;
+    const { hide, address } = this.props;
+    return hide ? null : (
+      <a className="link" href={address}>Redirect</a>
+    );
   }
 }
 
