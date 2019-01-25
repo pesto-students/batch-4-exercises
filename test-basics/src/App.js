d5c00a21fe1c38bbcd78915a189c2db268a91457
Aditya Saxena <adityasmksaxena@gmail.com>
2019-01-26 00:08:47 +0530

    complete exercise test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 /* eslint-disable react/no-multi-comp, react/no-unused-state */
 class App extends Component {
-  state = {
-    on: false,
-    input: '',
-    mainColor: 'blue',
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      on: false,
+      input: "",
+      mainColor: "blue"
+    };
+  }
+  handleStrings(string) {
+    return !string;
+  }
+  handleInput(event) {
+    const { value: input } = event.target;
+    this.setState(() => ({ input }));
+  }
+  handleClick(event) {
+    this.setState(previousState => ({ on: !previousState.on }));
   }
   render() {
+    const { input, mainColor, on } = this.state;
     return (
       <div className="App">
-        Welcome to React
+        <h1 className={`${mainColor}`}>Welcome to React</h1>
+        <h2>{input}</h2>
+        <input type="text" value={input} onChange={this.handleInput} />
+        <button onClick={this.handleClick}>
+          <p className={"button-state"}>{on ? "Yes!" : "No!"}</p>
+        </button>
       </div>
     );
   }
@@ -20,7 +40,9 @@ class App extends Component {
 
 export class Link extends Component {
   render() {
-    return null;
+    const { hide, address } = this.props;
+    if (hide) return null;
+    return <a href={address}>link</a>;
   }
 }
 
