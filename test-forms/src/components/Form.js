fb91aa285890a767a9a3c37d4135dd98a391ad88
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-26 00:19:36 +0530

    more

--- test-forms/src/components/Form.js
+++ test-forms/src/components/Form.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../styles/Form.css';
+import "../styles/Form.css";
 // import api from '../api';
 
 export default class Form extends Component {
-  state = {
-    name: '',
+  constructor(props) {
+    super(props);
+    state = {
+      name: "",
+      receivePromotions: true
+    };
+    this.receivePromotions = this.receivePromotions.bind(this);
   }
+
+  handlePromotions() {
+    this.setState(state => ({ receivePromotions: !state.receivePromotions }));
+  }
+
   render() {
     return (
       <form data-testid="addUserForm" onSubmit={this.handleSubmit}>
         <h2>Request Information</h2>
-        <input data-testid="name" type="text" placeholder="Name" value={this.state.name} />
+        <input
+          data-testid="name"
+          type="text"
+          placeholder="Name"
+          value={this.state.name}
+        />
         <div>
-          <input data-testid="checked" type="checkbox" />
-          <p data-testid="promotionsP" className="promotions">Receive Promotions</p>
+          <input
+            checked={this.state.receivePromotions}
+            data-testid="checked"
+            type="checkbox"
+            onClick={this.handlePromotions}
+          />
+          <p data-testid="promotionsP" className="promotions">
+            Receive Promotions
+          </p>
         </div>
-        <button type="submit" data-testid="submitButton">Submit</button>
+        <button type="submit" data-testid="submitButton">
+          Submit
+        </button>
       </form>
     );
   }
