b9d465e7c2463c92f014bdb82140b48921a35024
Prakhar Yadav <prakhary95@gmail.com>
2019-01-29 00:37:03 +0530

    Implement lastIndexOf

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,19 @@
+const getNameIfFunction = (item) => {
+  if (typeof item === 'function') {
+    return item.name;
+  }
 
-function lastIndexOf(...args) {
-  return args;
+  return item;
+};
+
+function lastIndexOf(searchItem, list) {
+  return list.reduce((acc, el, index) => {
+    if (getNameIfFunction(el) === getNameIfFunction(searchItem)) {
+      return index;
+    }
+
+    return acc;
+  }, -1);
 }
 
 export {
