09573afd9aef04a6c871c6b7ff82472f3746ff03
Prakhar Yadav <prakhary95@gmail.com>
2019-01-29 01:13:19 +0530

    Implement update-object

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,6 +1,26 @@
 
+const sanitizeIndex = (indexToSanitize, lengthOfArray) => {
+  if (indexToSanitize < 0) {
+    return lengthOfArray + indexToSanitize;
+  }
+
+  return indexToSanitize;
+};
+
 function updateObject(...args) {
-  return args;
+  if (args.length === 3) {
+    const [indexToUpdateAt, valueToUpdateWith, arrayToUpdate] = args;
+    const sanitizedIndex = sanitizeIndex(indexToUpdateAt, arrayToUpdate.length);
+
+    return arrayToUpdate.map((el, index) => {
+      if (index === sanitizedIndex) {
+        return valueToUpdateWith;
+      }
+
+      return el;
+    });
+  }
+  return (...moreArgs) => updateObject(...args, ...moreArgs);
 }
 
 export {
