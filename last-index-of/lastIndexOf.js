e14fcabd9b6996431e531392bcfb5ae36fcc211d
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-29 00:45:31 +0530

    more

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,8 +1,16 @@
+const getLastIndex = (list, itemToFind, foundIndex, prevIndex) => {
+  if (foundIndex === -1) {
+    return prevIndex;
+  }
 
-function lastIndexOf(...args) {
-  return args;
+  let newPrevIndex = -1;
+  const newIndex = list.slice(foundIndex + 1).findIndex(element => element === itemToFind);
+  if (newIndex !== -1) newPrevIndex = newIndex;
+  return getLastIndex(list, itemToFind, newIndex, newPrevIndex);
+};
+
+function lastIndexOf(itemToFind, list) {
+  return getLastIndex(list, itemToFind, 0, -1);
 }
 
-export {
-  lastIndexOf,
-};
+export { lastIndexOf };
