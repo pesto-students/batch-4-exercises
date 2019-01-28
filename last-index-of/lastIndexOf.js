74835f77e53586b4a6db685392ff6db01b8a292b
Nitin Patel <nitinpatel278@gmail.com>
2019-01-29 00:46:05 +0530

    WIPcommit

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,8 +1,10 @@
-
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(value, list) {
+  for (let i = list.length - 1; i > 0; i -= 1) {
+    if (list[i] === value) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-export {
-  lastIndexOf,
-};
+export { lastIndexOf };
