af84169d57285f8c6a1a95b103beda40d84e912f
jithintomz <jithintom1@gmail.com>
2019-01-29 00:36:00 +0530

    day-16-segment-1

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,13 @@
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(elementToBeSearched, array) {
+  const reducer = (lastIndex, element, index) => {
+    if (elementToBeSearched === element) {
+      return index;
+    }
+    return lastIndex;
+  };
+
+  return array.reduce(reducer, -1);
 }
 
 export {
