527ba6b8529150776fdcf50ada45f33a1f9d7a26
Prakhar Yadav <prakhary95@gmail.com>
2019-02-05 00:34:59 +0530

    Implement filtered-names

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,19 @@
+import fs from 'fs';
 
-function filteredNames(...args) {
-  return args;
+function filteredNames(directory, extension) {
+  const filesWithMatchingExtension = [];
+
+  const contents = fs.readdirSync(directory);
+  /* eslint-disable-next-line no-useless-escape */
+  const extensionRegEx = new RegExp(`\.${extension}$`);
+
+  contents.forEach((content) => {
+    if (content.match(extensionRegEx)) {
+      filesWithMatchingExtension.push(content);
+    }
+  });
+
+  return filesWithMatchingExtension;
 }
 
 export {
