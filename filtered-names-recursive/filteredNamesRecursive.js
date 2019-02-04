563a269da452fb389f983234a514c958382e8618
Nitin Patel <nitinpatel278@gmail.com>
2019-02-05 00:30:06 +0530

    WIPcommit

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,28 @@
+import fs from 'fs';
+import path from 'path';
 
-function filteredNamesRecursive(...args) {
-  return args;
+const getLastElement = array => array[array.length - 1];
+const getFileExtensionFromName = fileName => getLastElement(fileName.split('.'));
+
+const getFilteredNames = (providedPath, extension, filesFound) => {
+  const allSubDirsAndFiles = fs.readdirSync(providedPath);
+
+  for (const fileOrDir of allSubDirsAndFiles) {
+    const fullPath = path.join(providedPath, fileOrDir);
+    const isDirectory = fs.lstatSync(fullPath).isDirectory();
+
+    if (isDirectory) {
+      getFilteredNames(fullPath, extension, filesFound);
+    } else if (getFileExtensionFromName(fileOrDir) === extension) {
+      filesFound.push(fileOrDir);
+    }
+  }
+
+  return filesFound;
+};
+
+function filteredNamesRecursive(providedPath, extension) {
+  return getFilteredNames(providedPath, extension, []);
 }
 
-export {
-  filteredNamesRecursive,
-};
+export { filteredNamesRecursive };
