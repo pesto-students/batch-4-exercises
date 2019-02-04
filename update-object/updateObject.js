2afd0afd71cce85949f50b6d59c803ad68368a28
jithintomz <jithintom1@gmail.com>
2019-02-05 00:06:24 +0530

    day-16-segment-1

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,6 +1,12 @@
 
-function updateObject(...args) {
-  return args;
+function updateObject(index, elementToBeInserted, array) {
+  if (index > (array.length - 1)) {
+    return array;
+  }
+  const indexCpy = index < 0 ? array.length + index : index;
+  const arrayCpy = [...array];
+  arrayCpy[indexCpy] = elementToBeInserted;
+  return arrayCpy;
 }
 
 export {
