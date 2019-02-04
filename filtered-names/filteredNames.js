17294877254f1b4bac37a7a890c15d00f71c6283
jithintomz <jithintom1@gmail.com>
2019-02-05 00:47:57 +0530

    day-17-segment-1

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNames(...args) {
-  return args;
+function filteredNames(folderPath, extension) {
+  let allFiles = [];
+
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return [];
+    }
+    for (const file of files) {
+      if (fs.lstatSync(path.join(folderPath, file)).isDirectory()) {
+        filteredNames(path.join(folderPath, file), extension);
+        allFiles = allFiles.concat(filteredNames(folderPath, extension));
+      }
+      if (path.extname(file) === extension) {
+        allFiles.push(file);
+      }
+    }
+    return allFiles;
+  });
+  return allFiles;
 }
 
 export {
