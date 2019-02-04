3d00e3cac176ac459d11c18207054b635094f09c
Nitin Patel <nitinpatel278@gmail.com>
2019-02-05 00:33:24 +0530

    WIPcommit

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,8 +1,24 @@
+import fs from 'fs';
+import path from 'path';
 
-function filteredNames(...args) {
-  return args;
+const not = condition => !condition;
+const getLastElement = array => array[array.length - 1];
+const getFileExtensionFromName = fileName => getLastElement(fileName.split('.'));
+
+function filteredNames(providedPath, extension) {
+  const filesFound = [];
+  const allSubDirsAndFiles = fs.readdirSync(providedPath);
+
+  for (const fileOrDir of allSubDirsAndFiles) {
+    const fullPath = path.join(providedPath, fileOrDir);
+    const isDirectory = fs.lstatSync(fullPath).isDirectory();
+
+    if (not(isDirectory) && getFileExtensionFromName(fileOrDir) === extension) {
+      filesFound.push(fileOrDir);
+    }
+  }
+
+  return filesFound;
 }
 
-export {
-  filteredNames,
-};
+export { filteredNames };
