46db93c383f24155ad6b35462b1074a3d9a8b0fa
Aditya Saxena <adityasmksaxena@gmail.com>
2019-02-05 04:44:56 +0530

    [WIP] Day 17 Segment 2

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,24 @@
+const https = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+const getOptions = {
+  host: 'api.coinmarketcap.com',
+  port: 80,
+  path: '/v2/ticker/',
+  method: 'GET',
+};
+
+function bitcoinTicker() {
+  return new Promise((response, reject) => {
+    const reqGet = https.request(getOptions, (res) => {
+      res.on('data', (d) => {
+        response(d);
+      });
+    });
+    reqGet.end();
+    reqGet.on('error', (e) => {
+      reject(e);
+    });
+  });
 }
 
-export {
-  bitcoinTicker,
-};
+export { bitcoinTicker };
