7767913ac5ac71979afbef00735d36a6db8a96ed
jithintomz <jithintom1@gmail.com>
2019-02-05 04:57:01 +0530

    day-17-segment-2

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,30 @@
+const fs = require('fs');
+const readLine = require('readline');
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(csvFile) {
+  const jsonOutput = [];
+  let lineIndex = 1;
+  let columns = [];
+  const readLineInterface = readLine.createInterface({
+    input: fs.createReadStream(csvFile),
+    output: process.stdout,
+    console: false,
+  });
+  readLineInterface.on('line', (line) => {
+    const jsonRow = {};
+    if (lineIndex === 0) {
+      columns = line.split(',');
+    } else {
+      const rowValues = line.split(',');
+      for (const [index, column] of columns.entries()) {
+        jsonRow[column] = rowValues[index];
+      }
+      jsonOutput.push(jsonRow);
+    }
+    lineIndex += 1;
+  });
+
+  return jsonOutput;
 }
 
 export {
