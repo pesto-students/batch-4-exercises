ab4f1e16af306d745a402ea1aa10da10c1abf113
Nitin Patel <nitinpatel278@gmail.com>
2019-02-05 04:37:30 +0530

    WIPcommit

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,33 @@
+import https from 'https';
 
-function bitcoinTicker(...args) {
-  return args;
+const apiEndPoint = 'https://api.coinmarketcap.com/v2/ticker/';
+
+const convertObjToQueryString = (paramObject) => {
+  let queryString = '?';
+  for (const [key, value] of Object.entries(paramObject)) {
+    queryString += `${key}=${value}`;
+  }
+
+  return queryString;
+};
+
+function bitcoinTicker(params) {
+  const queryString = convertObjToQueryString(params);
+
+  return new Promise((resolve) => {
+    https.get(`${apiEndPoint}${queryString}`, { query: params }, (res) => {
+      let response = '';
+      res.setEncoding('utf-8');
+
+      res.on('data', (chunk) => {
+        response += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(JSON.parse(response));
+      });
+    });
+  });
 }
 
-export {
-  bitcoinTicker,
-};
+export { bitcoinTicker };
