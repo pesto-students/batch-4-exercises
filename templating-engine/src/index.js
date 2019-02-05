d4423580c7420f102ac7b6c29f8766f365fb12e0
jithintomz <jithintom1@gmail.com>
2019-02-06 00:47:03 +0530

    day-18-segment-1

--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const ejs = require("ejs");
+const ejs = require('ejs');
 
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
-app.get('/', (req, res) => {
-  return res.send(res.render('index.ejs')); // Use res.render() to render the ejs file instead of sending text response
-});
+app.get('/', (req, res) => res.render('index.ejs', { languages }));
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
