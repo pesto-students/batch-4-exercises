c8b4332fd4c27ef8491382f0f0a598b45db39982
Prakhar Yadav <prakhary95@gmail.com>
2019-02-06 00:21:40 +0530

    Implement express-forms

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,19 +1,44 @@
+/* eslint-disable import/no-unresolved */
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.set('view engine', 'ejs');
+
+const parseParams = bodyParser.urlencoded({ extended: false });
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.get('/submit-form-with-get', parseParams, (req, res) => {
+  const { first: firstName, last: lastName } = req.query;
+
+  const response = {
+    firstName,
+    lastName,
+  };
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(JSON.stringify(response));
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
+app.post('/submit-form-with-post', parseParams, (req, res) => {
+  const { first: firstName, last: lastName } = req.body;
 
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+  const response = {
+    firstName,
+    lastName,
+  };
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(JSON.stringify(response));
 });
 
 app.listen(3000, () => {
+  /* eslint-disable-next-line no-console */
   console.log('Server is listening on port 3000');
 });
