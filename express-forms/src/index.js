5bbd64c50a4bb604cb9f2bd65b0eafb0910f798a
jithintomz <jithintom1@gmail.com>
2019-02-06 00:36:48 +0530

    day-18-segment-1

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser());
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/submit-form-with-get/', (req, res) => {
+  res.send({ firstName: req.query.first, lastName: req.query.last });
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
+app.post('/submit-form-with-post/', (req, res) => {
+  res.send({ firstName: req.body.first, lastName: req.body.first });
 });
-
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
-});
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
