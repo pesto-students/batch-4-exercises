60325ba838e60e414501cb8c58b530c6e98f5656
jithintomz <jithintom1@gmail.com>
2019-02-06 00:07:28 +0530

    day-18-segment-1

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// 1) Create Routes
+const port = 3000;
 
-// 2) Start server on port 3000
+app.get('/', (req, res) => res.send('Hey, I am server response'));
+
+app.get('/movie/:title', (req, res) => {
+  res.send(`My favorite movie is ${req.params.title}`);
+});
+
+app.listen(port, () => {
+  console.log('Listening on port 3000');
+});
 
 module.exports = app;
--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const ejs = require("ejs");
 
 const app = express();
 
+app.set('view engine', 'ejs')
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
 app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
+  return res.send(res.render('index.ejs')); // Use res.render() to render the ejs file instead of sending text response
 });
 
 app.listen(3000, () => {
