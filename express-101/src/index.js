25694ffe6b7d6a8964b4f399f01a2eac3820335e
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-06 00:24:06 +0530

    Complete express-101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
-// 1) Create Routes
 
-// 2) Start server on port 3000
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
+
+// returns the favorie movie name given in query parameter in url
+app.get('/movie/:title', (req, res) => {
+  const { title } = req.params;
+  res.send(`My favorite movie is ${title}`);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port:${PORT}`);
+});
 
 module.exports = app;
