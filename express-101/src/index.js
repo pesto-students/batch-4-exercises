ab251fd5934f448c77aa9a4c6d3e5c465af6ded5
Prakhar Yadav <prakhary95@gmail.com>
2019-02-05 23:42:56 +0530

    Implement express-101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,22 @@
+
+/* eslint-disable-next-line import/no-unresolved */
 const express = require('express');
 
 const app = express();
 
-// 1) Create Routes
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
 
-// 2) Start server on port 3000
+app.get('/movie/:title', (req, res) => {
+  const { title } = req.params;
+
+  res.send(`My favorite movie is ${title}`);
+});
+
+app.listen(3000, () => {
+  /* eslint-disable-next-line no-console */
+  console.log('Server listening on port: 3000');
+});
 
 module.exports = app;
