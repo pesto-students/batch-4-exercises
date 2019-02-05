dcfc241beb155c512b1e40321058afc1bcacc662
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-06 00:24:58 +0530

    Complete express-forms

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express');
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/../views'));
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
@@ -8,6 +13,16 @@ app.get('/', (req, res) => {
 
 app.get('/form-with-get', (req, res) => {
   return res.render('form-with-get');
+});
+
+app.get('/submit-form-with-get', (req, res) => {
+  const { first, last } = req.query;
+  res.send({ firstName: first, lastName: last });
+});
+
+app.post('/submit-form-with-post', (req, res) => {
+  const { first, last } = req.body;
+  res.send({ firstName: first, lastName: last });
 });
 
 app.get('/form-with-post', (req, res) => {
