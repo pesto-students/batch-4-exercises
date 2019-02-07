7a6c8f298b62dba68262a546ba388d26f639bce5
Nitin Patel <nitinpatel278@gmail.com>
2019-02-08 00:46:52 +0530

    WIPcommit

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,15 +1,18 @@
 const isInAlphabet = character => character.match(/\W/);
-const isEmpty = list => list.length === 0;
 const getLastChar = string => string.substr(-1);
 const lastCharacterIsInAlphabet = string => isInAlphabet(getLastChar(string));
 
 function titleize(string) {
-  return Array.from(string).reduce((acc, cu) => {
-    if (lastCharacterIsInAlphabet(acc) || isEmpty(acc)) {
-      return acc + cu.toUpperCase();
+  return Array.from(string).reduce((accumulator, currentChar) => {
+    if (accumulator === '') {
+      return currentChar.toUpperCase();
     }
 
-    return acc + cu.toLowerCase();
+    if (lastCharacterIsInAlphabet(accumulator)) {
+      return accumulator + currentChar.toUpperCase();
+    }
+
+    return accumulator + currentChar.toLowerCase();
   }, '');
 }
 
