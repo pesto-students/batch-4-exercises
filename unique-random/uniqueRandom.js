73cbece22c7583e4401eddead3e909eb6871aa36
Prakhar Yadav <prakhary95@gmail.com>
2019-02-08 00:33:33 +0530

    Implement unique-random

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,6 +1,16 @@
 
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(start, end, except = null) {
+  let lastResult = except;
+  const effectiveRange = end - start + 1;
+
+  return () => {
+    const result = Math.ceil(Math.random() * effectiveRange);
+    if (result !== lastResult) {
+      lastResult = result;
+      return result;
+    }
+    return uniqueRandom(start, end, result);
+  };
 }
 
 export {
