a47806feff58cdb6e4dc9804c3d9c4207ec0d747
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-08 00:36:38 +0530

    [WIP]: COMPLETE day 20 segment 1

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,20 @@
+const capitalizeWord = word => word.charAt(0).toUpperCase() + word.slice(1);
 
-function titleize(...args) {
-  return args;
+function titleize(originalString) {
+  const wordDistincterInString = originalString.split('').find((char) => {
+    const asciiValue = char.charCodeAt(0);
+    if (((65 <= asciiValue) && (asciiValue <= 90)) || ((97 <= asciiValue) && (asciiValue <= 122))) {
+      return false;
+    }
+    return true;
+  });
+  const splittedStringOnNonChar = originalString.toLowerCase().split(/\W/);
+  const firstWord = capitalizeWord(splittedStringOnNonChar.shift());
+  const capitalizedStringVersion = splittedStringOnNonChar.reduce((acc, word) => {
+    acc += wordDistincterInString + capitalizeWord(word);
+    return acc;
+  }, firstWord);
+  return capitalizedStringVersion;
 }
 
 export {
