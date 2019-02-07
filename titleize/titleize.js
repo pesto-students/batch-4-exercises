f68bee6318826fa06c19e52c7726819083b48ebd
Aditya Saxena <adityasmksaxena@gmail.com>
2019-02-08 00:24:42 +0530

    completed exercise titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,8 +1,21 @@
-
-function titleize(...args) {
-  return args;
+const isCharacter = char => char.length === 1 && char.match(/[a-z]/i);
+function titleize(words) {
+  let shouldCapitalize = true;
+  let newSentence = '';
+  for (const char of words) {
+    if (isCharacter(char)) {
+      if (shouldCapitalize) {
+        newSentence += char.toUpperCase();
+      } else {
+        newSentence += char.toLowerCase();
+      }
+      shouldCapitalize = false;
+    } else {
+      newSentence += char;
+      shouldCapitalize = true;
+    }
+  }
+  return newSentence;
 }
 
-export {
-  titleize,
-};
+export { titleize };
