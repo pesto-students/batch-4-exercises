2cb4f1771ee2e782e26fcea9d8c014cc84be8d61
Aditya Saxena <adityasmksaxena@gmail.com>
2019-02-08 00:38:12 +0530

    completed exercise unique-random

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,8 +1,17 @@
-
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(fromVal, toVal) {
+  const diff = toVal - fromVal + 1;
+  let lastVal = -1;
+  function getRandomVal() {
+    return Math.floor(Math.random() * diff) + fromVal;
+  }
+  return function createRandom() {
+    const newRandomVal = getRandomVal();
+    if (newRandomVal !== lastVal) {
+      lastVal = newRandomVal;
+      return newRandomVal;
+    }
+    return createRandom();
+  };
 }
 
-export {
-  uniqueRandom,
-};
+export { uniqueRandom };
