e0281592a3f58d27d1cee16253a2f293e5d89138
Prakhar Yadav <prakhary95@gmail.com>
2019-02-08 00:24:56 +0530

    Implement titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,25 @@
+const isNotALetter = character => !character.match(/[a-zA-Z]/);
 
-function titleize(...args) {
-  return args;
+const lastCharacter = string => string[string.length - 1];
+
+function titleize(sentence) {
+  const characters = sentence.toLowerCase().split('');
+
+  return characters.reduce((acc, character, index) => {
+    const lastCharacterInResult = lastCharacter(acc);
+
+    let characterToAdd;
+
+    if (index === 0) {
+      characterToAdd = character.toUpperCase();
+    } else if (isNotALetter(lastCharacterInResult)) {
+      characterToAdd = character.toUpperCase();
+    } else {
+      characterToAdd = character;
+    }
+
+    return acc + characterToAdd;
+  }, '');
 }
 
 export {
